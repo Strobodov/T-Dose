b48c275c2ced154064cfb91307df588132b6c28c
Marcel Kornegoor <m.kornegoor@atcomputing.nl>
2023-04-22 22:55:48 +0200

    Hello, T-DOSE!

--- minimal.sh
+++ minimal.sh
@@ -12,7 +12,7 @@ ssh tdose@tdose.westeurope.cloudapp.azure.com -o StrictHostKeyChecking=no <<EOF
     sudo apt update
     sudo apt-get install docker.io --assume-yes
     git clone https://github.com/Strobodov/T-Dose.git
-    sudo docker build App/ --tag=mywebapp
+    sudo docker build T-Dose/App/ --tag=mywebapp
     sudo docker run -d -p 80:80 mywebapp
 EOF
 firefox http://tdose.westeurope.cloudapp.azure.com
